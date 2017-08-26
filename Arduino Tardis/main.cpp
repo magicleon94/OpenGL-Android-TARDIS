717d50ca6df1a02053aba4925251e86bd70256a6
magicleon <magicleon94@gmail.com>
2017-08-26 19:08:15 +0200

    Initial Commit

---
+++ Arduino Tardis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Arduino Tardis
+//
+//  Created by Antonello Galipò on 26/08/17.
+//  Copyright © 2017 Antonello Galipò. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
